b6e0e1775a056b27c38b38a39dcca01d2bf1cd76
zazie-me <nethmadhu01@gmail.com>
2026-03-31 05:11:32 +0530

    Update index.ts

--- src/Socket/index.ts
+++ src/Socket/index.ts
@@ -4,6 +4,20 @@ import { makeCommunitiesSocket } from './communities'
 
 // export the last socket layer
 const makeWASocket = (config: UserFacingSocketConfig) => {
+	// --- ZAZIE-MD CUSTOM BANNER ---
+	console.log(`
+\x1b[36m /$$$$$$$$  /$$$$$$  /$$$$$$$$ /$$ /$$$$$$$$          /$$      /$$ /$$$$$$$ \x1b[0m
+\x1b[36m|____ /$$/ /$$__  $$|____ /$$/|__/| $$_____/          | $$$    /$$$| $$__  $$\x1b[0m
+\x1b[36m    /$$/  | $$  \\ $$    /$$/   /$$| $$                | $$$$  /$$$$| $$  \\ $$\x1b[0m
+\x1b[36m   /$$/   | $$$$$$$$   /$$/   | $$| $$$$$    /$$$$$$  | $$ $$ / $$| $$  | $$\x1b[0m
+\x1b[36m  /$$/    | $$__  $$  /$$/    | $$| $$__/   |______/  | $$  $$$ | $$| $$  | $$\x1b[0m
+\x1b[36m /$$/     | $$  | $$ /$$/      | $$| $$                | $$ \\  $ | $$| $$  | $$\x1b[0m
+\x1b[36m/$$$$$$$$ | $$  | $$ /$$$$$$$$| $$| $$$$$$$$          | $$  \\    | $$| $$$$$$$/\x1b[0m
+\x1b[36m|________/ |__/  |__/|________/|__/|________/          |__/      |__/|_______/ \x1b[0m
+                                                                                
+\x1b[33m 🚀 Baileys Layer Initialized by Zazie-MD \x1b[0m
+	`);
+
 	const newConfig = {
 		...DEFAULT_CONNECTION_CONFIG,
 		...config
